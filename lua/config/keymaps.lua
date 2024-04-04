bd0b15c8eca0c21df81a7496ed65215d461c8ec1
Aleksandr Vlasenko <77232221+ayuvlasenko@users.noreply.github.com>
2024-04-04 06:21:33 +0400

    feat: remove useless keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll and center screen" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll and center screen" })
 vim.keymap.set("n", "<C-j>", "i<CR><Esc>l", { desc = "Break a line" })
-vim.keymap.set("n", "F8", function()
-  vim.diagnostic.goto_next()
-end, {
-  desc = "Go to next diagnostic",
-})
