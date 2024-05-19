51a343e33c20a290a7f4d3fe67df138cf86b3e5b
Aleksandr Vlasenko <77232221+ayuvlasenko@users.noreply.github.com>
2024-05-19 15:56:48 +0400

    feat: improve vscode mappings

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -11,29 +11,28 @@ opt.smartcase = true
 opt.hlsearch = false
 opt.wrapscan = true
 
--- useful mappings
 map("n", "<C-j>", "i<CR><Esc>^", { desc = "Break a line" })
+map(
+  "n",
+  "<leader>lr",
+  [[<Cmd>lua require('vscode-neovim').call('editor.action.rename')<CR>]],
+  { noremap = true, desc = "Rename" }
+)
 
--- tabs navigation
-map({ "n", "x" }, "<S-h>", ":tabprevious<CR>", { desc = "Prev tab" })
-map({ "n", "x" }, "<S-l>", ":tabnext<CR>", { desc = "Next tab" })
-map({ "n", "x" }, "<leader>c", ":tabclose<CR>", { desc = "Close tab" })
-
--- code navigation
+map({ "n", "x" }, "<S-h>", "gT", { desc = "Prev tab", remap = true })
+map({ "n", "x" }, "<S-l>", "gt", { desc = "Next tab", remap = true })
+map({ "n", "x" }, "<leader>c", "<C-w>c", { desc = "Close window", remap = true })
+map({ "n", "x" }, "<leader>bd", "<C-w>c", { desc = "Close window", remap = true })
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true })
 map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true })
 
--- windows navigation
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+map("v", "<", "<gv", { desc = "Indent left" })
+map("v", ">", ">gv", { desc = "Indent right" })
 
--- return to visual mode after indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
-
--- add bindings to accept copilot suggestions
 map(
   "i",
   "<C-u>",
@@ -47,7 +46,6 @@ map(
   { noremap = true, desc = "Accept next word from Copilot suggestion" }
 )
 
--- other vscode bindings
 map(
   "n",
   "<leader>e",
