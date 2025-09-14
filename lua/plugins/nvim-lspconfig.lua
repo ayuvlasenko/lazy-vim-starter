83af00f6ad06e8fd606a5c943fbcf5a499ed923b
Aleksandr Vlasenko <77232221+ayuvlasenko@users.noreply.github.com>
2025-09-14 14:54:39 +0400

    feat: add config for vtsls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,20 @@ return {
   opts = {
     servers = {
       prismals = {},
+      vtsls = {
+        settings = {
+          javascript = {
+            tsserver = {
+              maxTsServerMemory = 16184,
+            },
+          },
+          typescript = {
+            tsserver = {
+              maxTsServerMemory = 16184,
+            },
+          },
+        },
+      },
     },
     inlay_hints = { enabled = false },
   },
